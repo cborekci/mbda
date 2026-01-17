8317860ec1af74b4a7e7fca1dd5169c67a76c918
cborekci <58049262+cborekci@users.noreply.github.com>
2026-01-17 21:24:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if uploaded_file and api_key:
         # --- 4. ANALİZ İŞLEMİ ---
         if st.button("🚀 Analizi Başlat"):
             genai.configure(api_key=api_key)
-            model = genai.GenerativeModel('gemini-pro')
+            model = genai.GenerativeModel('gemini-2.5-flash')
 
             with st.spinner('Yapay zeka verileri okuyor, kodluyor ve analiz ediyor...'):
                 
@@ -178,5 +178,6 @@ elif not api_key:
 
 elif not uploaded_file:
     st.info("Lütfen analiz edilecek CSV dosyasını yükleyin.")
+
 
 
